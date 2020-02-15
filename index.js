8b66f8222e226c417808c4a0044a301f9adb6af6
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-02-15 13:33:57 +0530

    :sparkles: api to copy score from another ref working, when passing house and fromRef

--- index.js
+++ index.js
@@ -20,8 +20,33 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', function(req, res) {
+
+  var house = req.query.house;
+  console.log(house);
+  var from = req.query.from;
+  console.log(from);
+
+  var db = admin.database();
+  var fromRef = db.ref(from);
+  var toRef = db.ref('Testscore')
+
+  fromRef.once("value",function(snapshot){
+    
+    newValue = snapshot.child('House').child(house).val();
+    console.log(newValue);
+
+    toRef.child('House').child(house).set(newValue);
+
+
+  });
+
   res.send('coming soon');
 });
+
+
+function updateScore(String){
+
+}
 
 
 let server = app.listen(3000, function() {
